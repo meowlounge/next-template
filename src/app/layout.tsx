c7424588b067f18ef3a4485e0c0213adaa25612b
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-05-05 02:09:52 +0200

    Revert "feat: add color extraction feature with image upload and palette generation"
    This reverts commit 22f06ead50643dda423d1cea50850f704bfb50cb.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 
 import { DottedBackground } from '@/components/eagle/dotted';
 import { ReactScan } from '@/components/eagle/react-scan';
-import { Toaster } from '@/components/eagle/toast';
-import { Navbar } from '@/components/navbar';
+import { ThemeToggle } from '@/components/eagle/theme-toggle';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -19,16 +18,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-	title: 'prodbyeagle/color',
-	description:
-		'Blazing-fast, dependency-free TypeScript lib to extract dominant colors from images in RGB, HEX, HSL, and OKLCH formats.',
-	openGraph: {
-		title: 'prodbyeagle/color',
-		description:
-			'Extract, format & use colors from images on the front end with @prodbyeagle/color.',
-		url: 'https://github.com/prodbyeagle/color',
-		siteName: 'prodbyeagle/color',
-	},
+	title: '🦅 @prodbyeagle',
+	description: 'Eagle Template for Next.js',
 	icons: 'https://kappa.lol/WTiY5',
 };
 
@@ -40,25 +31,20 @@ export default function RootLayout({
 	return (
 		<html lang='en' suppressHydrationWarning>
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased bg-background text-foreground min-h-screen`}>
+				className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased select-none cursor-default`}>
 				<ThemeProvider
 					attribute='class'
 					defaultTheme='system'
 					enableSystem>
-					<div className='relative flex flex-col min-h-screen'>
-						<Navbar />
-
-						<main className='flex-1 flex items-start justify-center px-4 pb-20 pt-10'>
-							{children}
-						</main>
-
-						<DottedBackground
-							dotColor='var(--ring)'
-							spacing={16}
-							dotSize={1}
-						/>
+					{children}
+					<div className='absolute top-4 right-4'>
+						<ThemeToggle />
 					</div>
-					<Toaster />
+					<DottedBackground
+						dotColor='var(--ring)'
+						spacing={16}
+						dotSize={1}
+					/>
 				</ThemeProvider>
 				<ReactScan />
 			</body>
