6610b39173a1d229f37704350b4fa74f0d243861
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-05-23 17:06:11 +0200

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	devIndicators: false,
+	devIndicators: {
+		buildActivity: false,
+		appIsrStatus: false,
+	},
 };
 
 export default nextConfig;
