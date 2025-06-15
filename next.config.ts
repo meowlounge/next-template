18b38634f9807e20dbf37ae329f64204addbe282
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-06-15 02:53:08 +0200

    feat(redirects): add redirect for /contact to external URL ( shush... its rick )

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	devIndicators: false,
+	async redirects() {
+		return [
+			{
+				source: '/contact',
+				destination: 'https://kappa.lol/4BQFZj',
+				permanent: false,
+			},
+		];
+	},
 };
 
 export default nextConfig;
